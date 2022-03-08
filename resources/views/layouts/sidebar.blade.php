a2b77cf29b815eb1efa23a16428b66d4ad64c82c
AriSmrd7 <arismrd13@gmail.com>
2022-03-08 21:06:58 +0700

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,8 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <ul class="nav">
     <li class="nav-item nav-category">Main Menu</li>
+
+    @if(Auth::User()->hasRole('ROLE_ADMIN'))
     <li class="nav-item">
       <a class="nav-link" href="index.html">
         <i class="menu-icon typcn typcn-document-text"></i>
@@ -10,26 +12,60 @@
     <li class="nav-item">
       <a class="nav-link" href="pages/forms/basic_elements.html">
         <i class="menu-icon typcn typcn-shopping-bag"></i>
-        <span class="menu-title">Cucian Masuk</span>
+        <span class="menu-title">Data Pesanan</span>
       </a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="pages/charts/chartjs.html">
         <i class="menu-icon typcn typcn-th-large-outline"></i>
-        <span class="menu-title">Cucian Keluar</span>
+        <span class="menu-title">Data Transaksi</span>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="pages/tables/basic-table.html">
-        <i class="menu-icon typcn typcn-bell"></i>
-        <span class="menu-title">Transaksi</span>
+      <a class="nav-link" href="pages/charts/chartjs.html">
+        <i class="menu-icon typcn typcn-th-large-outline"></i>
+        <span class="menu-title">Data Jasa</span>
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="pages/icons/font-awesome.html">
-        <i class="menu-icon typcn typcn-user-outline"></i>
+      <a class="nav-link" href="pages/charts/chartjs.html">
+        <i class="menu-icon typcn typcn-th-large-outline"></i>
+        <span class="menu-title">Data Pewangi</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="pages/charts/chartjs.html">
+        <i class="menu-icon typcn typcn-th-large-outline"></i>
         <span class="menu-title">Data Pelanggan</span>
       </a>
     </li>
+    <li class="nav-item">
+      <a class="nav-link" href="pages/charts/chartjs.html">
+        <i class="menu-icon typcn typcn-th-large-outline"></i>
+        <span class="menu-title">Data Petugas</span>
+      </a>
+    </li>
+    @endif
+
+    @if(Auth::User()->hasRole('ROLE_KASIR'))
+    <li class="nav-item">
+      <a class="nav-link" href="index.html">
+        <i class="menu-icon typcn typcn-document-text"></i>
+        <span class="menu-title">Home</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="pages/forms/basic_elements.html">
+        <i class="menu-icon typcn typcn-shopping-bag"></i>
+        <span class="menu-title">Order Pesanan</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="pages/charts/chartjs.html">
+        <i class="menu-icon typcn typcn-th-large-outline"></i>
+        <span class="menu-title">Transaksi</span>
+      </a>
+    </li>
+   @endif
   </ul>
 </nav>
