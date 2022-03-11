b01e910bdda3cc3c28681686e08412dcbc5d339b
AriSmrd7 <arismrd13@gmail.com>
2022-03-11 23:05:16 +0700

    update

--- app/Http/Controllers/Kasir/OrderController.php
+++ app/Http/Controllers/Kasir/OrderController.php
@@ -46,72 +46,7 @@ class OrderController extends Controller
              
                                 
         return view('kasir.order.order',compact('order','pelanggan','jasa','pewangi','ordertemp','ordertotal','noinvoice'))
-            ->with('i', (request()->input('page', 1) - 1) * 5);        }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
- 
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Kasir\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Kasir\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Kasir\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Order $order)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Kasir\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Order $order)
-    {
-        //
+            ->with('i', (request()->input('page', 1) - 1) * 5);        
     }
 
     public function insertOrder(Request $request){
