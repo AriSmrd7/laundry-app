04285b5f3ce4532ee2ed6323aba0fadf9f92935a
AriSmrd7 <arismrd13@gmail.com>
2022-03-13 15:51:37 +0700

    update

--- app/Models/Admin/Member.php
+++ app/Models/Admin/Member.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+    use HasFactory;
+
+    protected $table = 'tb_member';
+    protected $keyType = 'string';
+    protected $fillable = [
+        'id_pelanggan',
+        'status_member',
+        'total_saldo',
+        'total_kg',
+    ];
 }
---
+++ app/Models/Admin/MemberDetail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MemberDetail extends Model
+{
+    use HasFactory;
+
+    public $table = 'tb_member_detail';
+    public $primaryKey = NULL;
+    public $incrementing = FALSE;
+    public $keyType = 'string';
+    public $fillable = [
+        'id_jasa',
+        'subtotal_kg',
+        'subtotal_saldo',
+    ];}
